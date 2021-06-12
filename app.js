c3b95955604cb34d15561d0ae86404b32095c522
root <asifmahmoodjabbar@gmail.com>
2021-06-12 09:19:19 +0200

    Initial setup of HTML pages define

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+app.use(express.static('public'));
+
+app.get("/", (req, res, next) => res.render("index"));
+app.get("/login", (req, res, next) => res.render("login"));
+
+
+
+
+
+
+app.listen(8000, () => console.log("Yes its working darling"));
